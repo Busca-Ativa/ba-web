3cdf57ca8b203d047ed8dce1f355fac357092458
Dante de Araújo <danteeng@hotmail.com>
2024-08-27 13:12:26 -0300

    feat(form): Salva o formulário no banco

--- src/app/Survey/page.tsx
+++ src/app/Survey/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { useCallback } from "react";
 import "survey-core/defaultV2.min.css";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
+import api from '../../services/api'
 
 const surveyJson = {
+  title: "Teste",
   elements: [
     {
       name: "FirstName",
@@ -16,12 +19,45 @@ const surveyJson = {
       title: "Enter your last name:",
       type: "text",
     },
+    {
+      name: "Resposta",
+      title: "Sua resposta",
+      type: "text",
+    },
   ],
 };
 
+const surveySchema = {
+  name: "Teste",
+  json: surveyJson
+}
+
+
 function App() {
   const survey = new Model(surveyJson);
+  const alertResults = useCallback(async (sender) => {
+    try {
+      const response = await api.post('/editor/form',
+        JSON.stringify(surveyJson),
+        {
+          withCredentials:true,
+          // headers: {
+          //   'Access-Control-Allow-Origin': '*',
+          //   'Content-Type': 'application/json'
+          // }
+        }
+      )
+      if ( response.data.data  ){
+        console.log(response.data.data)
+      }
+    } catch (error: any) {
+      console.error("Erro ao enviar formulário", error.response?.data || error.message)
+      throw error;
+    }
+    const results = JSON.stringify(sender.data);
+  }, []);
 
+  survey.onComplete.add(alertResults)
   return <Survey model={survey} />;
 }
 
