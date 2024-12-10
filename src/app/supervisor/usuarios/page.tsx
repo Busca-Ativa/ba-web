4dfde7d1096812b6c434654db8961452877e68c9
Yanna Torres <torres.yanna03@gmail.com>
2024-12-10 10:26:57 -0300

    feat: display only active users

--- src/app/supervisor/usuarios/page.tsx
+++ src/app/supervisor/usuarios/page.tsx
@@ -10,7 +10,7 @@ import { HowToReg } from "@mui/icons-material";
 import BATable from "@/components/BATable";
 import NewInstitutionModal from "@/components/Modals/NewInstitution";
 import { toast, ToastContainer } from "react-toastify";
-import { translateRole } from "@/utils/index"
+import { translateRole } from "@/utils/index";
 import CoordinatorEditUser from "@/components/Modals/CoordinatorEditUser";
 
 interface Row {
@@ -33,22 +33,28 @@ const UsuariosAdmin = () => {
   const handleShowApproval = () => setShowApprovalPage(true);
   const handleHideApproval = () => setShowApprovalPage(false);
 
-  const handleApproval = async (row: Record<string, string | number>, rowIndex: number, approval: boolean) => {
+  const handleApproval = async (
+    row: Record<string, string | number>,
+    rowIndex: number,
+    approval: boolean
+  ) => {
     try {
-      const response = await api.post('/supervisor/user/active', {id_user:row.id, active:approval},{withCredentials: true})
+      const response = await api.post(
+        "/supervisor/user/active",
+        { id_user: row.id, active: approval },
+        { withCredentials: true }
+      );
       const dataFromApi = response.data;
       if (response.status === 200) {
-        if (approval){
+        if (approval) {
           const newRows = [...userRows];
           const user: any = dataFromApi.data;
-          newRows.push(
-            {
-              ...user,
-              unit: user.unit? user.unit.name : "Pertence a essa unidade",
-              role: translateRole(user.role),
-              config: { analysable: false, editable: true, deletable: true },
-            },
-          )
+          newRows.push({
+            ...user,
+            unit: user.unit ? user.unit.name : "Pertence a essa unidade",
+            role: translateRole(user.role),
+            config: { analysable: false, editable: true, deletable: true },
+          });
           setUserRows(newRows);
           const newApprovalRows = [...approvalRows];
           newApprovalRows.splice(rowIndex, 1);
@@ -59,21 +65,24 @@ const UsuariosAdmin = () => {
           const deactivatedUser = newRows.splice(rowIndex, 1);
           setUserRows(newRows);
           const newApprovalRows = [...approvalRows];
-          newApprovalRows.push(deactivatedUser)
+          newApprovalRows.push(deactivatedUser);
           setApprovalRows(newApprovalRows);
           toast.success(dataFromApi.message);
-
         }
       }
     } catch (error) {
       console.log(error);
     }
+  };
 
-  }
-
-  const handleDelete = async (row: Record<string, string | number>, rowIndex: number) => {
+  const handleDelete = async (
+    row: Record<string, string | number>,
+    rowIndex: number
+  ) => {
     try {
-      const response = await api.delete(`/supervisor/user/${row.id}` ,{withCredentials: true})
+      const response = await api.delete(`/supervisor/user/${row.id}`, {
+        withCredentials: true,
+      });
       const dataFromApi = response.data;
       if (response.status === 200) {
         const newApprovalRows = [...approvalRows];
@@ -84,8 +93,7 @@ const UsuariosAdmin = () => {
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
 
   useEffect(() => {
     const token = nookies.get(null).access_token;
@@ -99,21 +107,23 @@ const UsuariosAdmin = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await api.get('/supervisor/users', {withCredentials: true})
+        const response = await api.get("/supervisor/users?active=1", {
+          withCredentials: true,
+        });
         const dataFromApi = response.data;
-        const rows = dataFromApi.data.map( (user: any) => {
+        const rows = dataFromApi.data.map((user: any) => {
           return {
             ...user,
-            unit: user.unit? user.unit.name : "Pertence a essa instituição",
+            unit: user.unit ? user.unit.name : "Pertence a essa instituição",
             role: translateRole(user.role),
             config: { analysable: false, editable: true, deletable: true },
-          }
-        } )
+          };
+        });
         setUserRows(rows);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     getData();
   }, []);
 
@@ -121,22 +131,24 @@ const UsuariosAdmin = () => {
     if (showApprovalPage) {
       const getData = async () => {
         try {
-          const response = await api.get('/supervisor/users?active=0', {withCredentials: true})
+          const response = await api.get("/supervisor/users?active=0", {
+            withCredentials: true,
+          });
           const dataFromApi = response.data;
-          const rows = dataFromApi.data.map( (user: any) => {
-            console.log(user)
+          const rows = dataFromApi.data.map((user: any) => {
+            console.log(user);
             return {
               ...user,
-              unit: user.unit? user.unit.name : "Pertence a essa instituição",
+              unit: user.unit ? user.unit.name : "Pertence a essa instituição",
               role: translateRole(user.role),
               config: { disapproved: true, approved: true },
-            }
-          } )
+            };
+          });
           setApprovalRows(rows);
         } catch (error) {
           console.log(error);
         }
-      }
+      };
       getData();
     }
   }, [showApprovalPage]);
@@ -144,14 +156,14 @@ const UsuariosAdmin = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await api.get('/all/user', {withCredentials: true})
+        const response = await api.get("/all/user", { withCredentials: true });
         const dataFromApi = response.data;
         const unit = dataFromApi.unit;
         setUnitInfo(unit.name);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     getData();
   }, []);
 
@@ -169,13 +181,12 @@ const UsuariosAdmin = () => {
             role: "string",
             active: 0,
             id_institution: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            id_unit: "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+            id_unit: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
           },
         },
-        {withCredentials: true},
+        { withCredentials: true }
       )
-      .then((response) => {
-      });
+      .then((response) => {});
   };
 
   const columns = [
@@ -197,14 +208,27 @@ const UsuariosAdmin = () => {
                   {unitInfo}
                 </h2>
               </div>
-              <Button onClick={handleShowApproval} className="h-[41px] px-4 py-2 bg-[#19B394] hover:bg-[--primary-dark] rounded justify-center items-center gap-3 inline-flex text-white">
-                <HowToReg/>
+              <Button
+                onClick={handleShowApproval}
+                className="h-[41px] px-4 py-2 bg-[#19B394] hover:bg-[--primary-dark] rounded justify-center items-center gap-3 inline-flex text-white"
+              >
+                <HowToReg />
                 <div className="text-white text-sm font-semibold font-['Source Sans Pro'] leading-[18px]">
                   Aprovar Usuário
                 </div>
               </Button>
             </div>
-            <BATable columns={columns} initialRows={userRows} onEdit={handleOpen} onDelete={(row: Record<string, string | number>, rowIndex: number) => {handleApproval(row,rowIndex,false)}} />
+            <BATable
+              columns={columns}
+              initialRows={userRows}
+              onEdit={handleOpen}
+              onDelete={(
+                row: Record<string, string | number>,
+                rowIndex: number
+              ) => {
+                handleApproval(row, rowIndex, false);
+              }}
+            />
           </div>
         </>
       )}
@@ -213,23 +237,30 @@ const UsuariosAdmin = () => {
           <div className="w-[100%] h-[100vh px-[45px] pt-[60px] flex flex-col gap-8 2xl:gap-10">
             <div className="flex justify-between">
               <h1>Pedidos de Cadastro</h1>
-              <Button onClick={handleHideApproval} className="h-[41px] px-4 py-2 bg-[#19b394] hover:bg-[--primary-dark] rounded justify-center items-center gap-3 inline-flex text-white">
+              <Button
+                onClick={handleHideApproval}
+                className="h-[41px] px-4 py-2 bg-[#19b394] hover:bg-[--primary-dark] rounded justify-center items-center gap-3 inline-flex text-white"
+              >
                 <div className="text-white text-sm font-semibold font-['Source Sans Pro'] leading-[18px]">
                   Voltar
                 </div>
               </Button>
             </div>
             <BATable
-            columns={columns}
-            initialRows={approvalRows}
-            onDisapprove={ (row: Record<string, string | number>, rowIndex: number) => {
-                handleDelete(row, rowIndex)
-              }
-            }
-            onApprove={ (row: Record<string, string | number>, rowIndex: number) => {
-                handleApproval(row, rowIndex, true)
-              }
-            }
+              columns={columns}
+              initialRows={approvalRows}
+              onDisapprove={(
+                row: Record<string, string | number>,
+                rowIndex: number
+              ) => {
+                handleDelete(row, rowIndex);
+              }}
+              onApprove={(
+                row: Record<string, string | number>,
+                rowIndex: number
+              ) => {
+                handleApproval(row, rowIndex, true);
+              }}
             />
           </div>
         </>
